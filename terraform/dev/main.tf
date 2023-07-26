e888e110ef38582fea2e85f7ad6e97187e17c3de
Jeremy Coando <killerpit02@yahoo.com>
2023-07-26 08:24:42 -0600

    init_atlantis

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -12,6 +12,7 @@ terraform {
 
 resource "aws_instance" "myec2vm" {
   ami           = data.aws_ami.amzlinux2.id
+# cool loops
 #   instance_type = var.instance_type
 #   instance_type = var.instance_type_list[1]
   instance_type = var.instance_type_map["dev"]
